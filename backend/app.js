f750dd85dba1c4545751897864a2c24948f609df
mdekabs <1mawoda@gmail.com>
2024-03-01 21:00:59 +0100

    secret key generator

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -7,14 +8,14 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ exptended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("DataBase is up and running!");
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`server is running on port ${port}`));
+    const port = process.env.PORT ||  3000;
+    app.listen(port, () => console.log(`server is running on port ${port}.`));
   }
   catch (error) {
     console.error("sorry could not connect to the DataBase!", error);
